5cdcc11df8b50c4da586c3bf5ebd68059444f234
Said-laasri <saidlaasri3@gmail.com>
2023-01-25 12:29:56 +0100

    Add UserForm and list Users

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React, { useState } from 'react';
-
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
+import React, { useState } from "react";
+import AddUserForm from "./components/User/AddUserForm";
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
+import UserList from "./components/User/UserList";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
   ]);
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
+  const [users, setUsers] = useState([{ name: "Max", age: 28, id: "u1" }]);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
       updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
       return updatedGoals;
     });
   };
 
   let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
   );
 
   if (courseGoals.length > 0) {
@@ -35,8 +38,25 @@ const App = () => {
     );
   }
 
+  const addUserHundler = (userName, userAge) => {
+    setUsers((prevUsers) => {
+      const updatedUsers = [...prevUsers];
+      updatedUsers.unshift({
+        name: userName,
+        age: userAge,
+        id: Math.random().toString(),
+      });
+      return updatedUsers;
+    });
+  };
   return (
     <div>
+      <section id="user-form">
+        <AddUserForm onAddUser={addUserHundler} />
+      </section>
+      <section id="users">
+        <UserList items={users} />
+      </section>
       <section id="goal-form">
         <CourseInput onAddGoal={addGoalHandler} />
       </section>
