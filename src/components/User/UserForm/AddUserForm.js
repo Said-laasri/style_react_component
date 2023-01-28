ed95549ab43a8ac71e85b16f5c490317b71bf95f
Said-laasri <saidlaasri3@gmail.com>
2023-01-28 15:52:54 +0100

    Set input to empty string after from submit

--- src/components/User/UserForm/AddUserForm.js
+++ src/components/User/UserForm/AddUserForm.js
@@ -7,16 +7,14 @@ const AddUserForm = (props) => {
   const [userAge, setUserAge] = useState("");
   const [isValid, setIsValid] = useState(true);
   const userNameChangeHandler = (event) => {
-    event.preventDefault();
-    if(event.target.value.trim().length > 0){
-        setIsValid(true);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
     }
     setUserName(event.target.value);
   };
   const userAgeChangeHandler = (event) => {
-    event.preventDefault();
-    if(event.target.value.trim().length > 0){
-        setIsValid(true);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
     }
 
     setUserAge(event.target.value);
@@ -25,23 +23,44 @@ const AddUserForm = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
     if (userName.trim().length === 0 || userAge.trim().length === 0) {
-        setIsValid(false);
+      setIsValid(false);
       return <p className="errormessage">Please input your Name-Age</p>;
     }
     if (userAge < 0) {
-        setIsValid(false);
+      setIsValid(false);
       return <p className="errormessage">Please Input Real Age</p>;
     }
-
+    setUserAge("");
+    setUserName("");
     props.onAddUser(userName, userAge);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <label style={{ color: !isValid ? "red": "#000"}} htmlFor="username">Username</label>
-      <input style={{ backgroundColor: !isValid ? "rgb(218, 118, 105)": "transparent"}} type="text" id="username" onChange={userNameChangeHandler} />
-      <label style={{ color: !isValid ? "red": "#000"}} htmlFor="age">Age (Years)</label>
-      <input style={{ backgroundColor: !isValid ? "rgb(218, 118, 105)": "transparent"}} type="number" id="age" onChange={userAgeChangeHandler} />
+      <label style={{ color: !isValid ? "red" : "#000" }} htmlFor="username">
+        Username
+      </label>
+      <input
+        style={{
+          backgroundColor: !isValid ? "rgb(218, 118, 105)" : "transparent",
+        }}
+        type="text"
+        id="username"
+        value={userName}
+        onChange={userNameChangeHandler}
+      />
+      <label style={{ color: !isValid ? "red" : "#000" }} htmlFor="age">
+        Age (Years)
+      </label>
+      <input
+        style={{
+          backgroundColor: !isValid ? "rgb(218, 118, 105)" : "transparent",
+        }}
+        type="number"
+        id="age"
+        value={userAge}
+        onChange={userAgeChangeHandler}
+      />
       <Button type="submit">Add User</Button>
     </form>
   );
