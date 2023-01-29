6afd29f9f67f79fb1bbdad7dbe2f83eae52efa36
Said-laasri <saidlaasri3@gmail.com>
2023-01-29 20:31:48 +0100

    Update userlist so each user can be deleted sepratly

--- src/components/User/UserList/UserList.js
+++ src/components/User/UserList/UserList.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import SingleUser from '../UserOne/SingleUser';
-import './UserList.css';
+import React from "react";
+import SingleUser from "../UserOne/SingleUser";
+import "./UserList.css";
 
 const UserList = (props) => {
-    return (
-        <ul className='userList'>
-            {props.items.map((user) => (
-                <SingleUser key={user.id} name={user.name} age={user.age} />
-            ))}
-        </ul>
-    )
-
+  return (
+    <ul className="userList">
+      {props.items.map((user) => (
+        <SingleUser
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          onDelete={props.onDeleteItem}
+          age={user.age}
+        >
+          {user.name}, {user.age} years old
+        </SingleUser>
+      ))}
+    </ul>
+  );
 };
-
 
 export default UserList;
