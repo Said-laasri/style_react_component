35e67fdeeb2a34ce962cb769f5c380cefb1b4d54
Said-laasri <saidlaasri3@gmail.com>
2023-01-31 18:43:58 +0100

    Add errorHundler

--- src/components/User/UserForm/AddUserForm.js
+++ src/components/User/UserForm/AddUserForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ErrorModal from "../../UI/Error/ErroModal";
 import Button from "../../UI/Button/Button";
 import "./AddUserForm.css";
 
@@ -6,6 +7,8 @@ const AddUserForm = (props) => {
   const [userName, setUserName] = useState("");
   const [userAge, setUserAge] = useState("");
   const [isValid, setIsValid] = useState(true);
+  const [error, setError] = useState();
+
   const userNameChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
@@ -24,18 +27,32 @@ const AddUserForm = (props) => {
     event.preventDefault();
     if (userName.trim().length === 0 || userAge.trim().length === 0) {
       setIsValid(false);
-      return <p className="errormessage">Please input your Name-Age</p>;
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
     }
     if (userAge < 0) {
       setIsValid(false);
-      return <p className="errormessage">Please Input Real Age</p>;
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
     }
     setUserAge("");
     setUserName("");
     props.onAddUser(userName, userAge);
   };
 
+  const errorHandler = () => {
+    setError(null);
+   };
+
   return (
+    <>
+    {error && <ErrorModal title={error.title} message={error.message}  onConfirm={errorHandler}/>}
     <form onSubmit={formSubmitHandler}>
       <label style={{ color: !isValid ? "red" : "#000" }} htmlFor="username">
         Username
@@ -63,6 +80,7 @@ const AddUserForm = (props) => {
       />
       <Button type="submit">Add User</Button>
     </form>
+    </>
   );
 };
 
